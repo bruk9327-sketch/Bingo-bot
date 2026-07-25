ac3e41018b708a5fa857ce326e1a3fca0da4929c
bruk9327-sketch <bruk9327@gmail.com>
2026-07-25 18:37:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import Application, CommandHandler, ContextTypes
 
