a6afaa98fd501e681dab81131f9a7eb365b194ee
bruk9327-sketch <bruk9327@gmail.com>
2026-08-18 16:53:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1905,4 +1905,4 @@ if __name__ == '__main__':
     socketio.start_background_task(game_loop)
     
     port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    socketio.run(app, host='0.0.0.0', port=port)
